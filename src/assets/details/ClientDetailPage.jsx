7c9a22c3ddbed8d5287488db9fb36d57874e3d55
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-06-23 16:28:58 +0200

    Aggiunta card

--- src/assets/details/ClientDetailPage.jsx
+++ src/assets/details/ClientDetailPage.jsx
@@ -1,7 +1,20 @@
+import { clients } from "../data/clients";
 export default function ClientDetailPage () {
     return (
-        <div className="container">
-            <h3><strong>Dettaglio partecipante:</strong> nome partecipante</h3>
-        </div>
+        <>
+            <div className="container">
+                <h3><strong>Dettaglio partecipante:</strong></h3>
+            </div>
+            <div class="card" style="width: 18rem;">
+                {clients.map((client) => {
+                    <div class="card-body" key={client.id}>
+                        <h5 class="card-title">{client.name}{client.surname}</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">{client.mail}</h6>
+                        <p class="card-phone">{client.phone}</p>
+                        <p class="card-code">{client.fiscalCode}</p>
+                    </div>
+                })}
+            </div>
+        </>
     )
 }
