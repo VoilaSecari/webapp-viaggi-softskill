cc7459c19bb6fefd521a7eb2d70dab36a04de71a
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-06-24 14:06:42 +0200

    aggiunte card in Homepage

--- src/assets/pages/Homepage.jsx
+++ src/assets/pages/Homepage.jsx
@@ -1,18 +1,67 @@
+
+
 export default function HomePage() {
   return (
-    <div className="container">
-      <h1 className="mt-4">Homepage</h1>
-      <div className="row pt-4">
-        <div className="col-4">
-          <div className="card-image w-20%">
-            immagine
+    <>
+      <div className="container-page">
+        <h1 className="mt-4">Homepage</h1>
+      </div>
+      <div className="row">
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/barcellona.jpg" alt="barcellona" />
           </div>
           <div className="card-body">
-            <h3 className="title">titolo</h3>
-            <p className="text">descrizione</p>
+            <h3>Barcellona, Spagna</h3>
+            <p>Barcellona, capoluogo della Catalogna, è una città spagnola vivace e cosmopolita, situata sulla costa nord-orientale del paese, vicino ai Pirenei e al confine con la Francia. È la seconda città più grande della Spagna, dopo Madrid, e un importante centro industriale, finanziario e culturale. Barcellona è rinomata per la sua architettura modernista, in particolare le opere di Antoni Gaudí, per le sue spiagge, la vivace vita notturna e la sua atmosfera unica che fonde tradizione e modernità. </p>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/Il Cairo.jpg" alt="Il Cairo" />
+          </div>
+          <div className="card-body">
+            <h3>Il Cairo, Egitto</h3>
+            <p>Il Cairo è la capitale dell'Egitto e una delle città più grandi e importanti del mondo, situata sul fiume Nilo. È un centro politico, amministrativo, culturale e turistico, famoso per la sua ricca storia, i monumenti antichi e la vibrante vita moderna. </p>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/New York.jpg" alt="New York" />
+          </div>
+          <div className="card-body">
+            <h3>New York, U.S.A</h3>
+            <p>New York è una metropoli globale, conosciuta come la "Grande Mela", e uno dei principali centri finanziari, commerciali e culturali del mondo. Situata sulla costa orientale degli Stati Uniti, alla foce del fiume Hudson, è composta da cinque distretti: Manhattan, Bronx, Queens, Brooklyn e Staten Island. New York è famosa per i suoi iconici grattacieli, musei, teatri, vivace vita notturna e multiculturalismo. </p>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/parigi-2.jpg" alt="Parigi" />
+          </div>
+          <div className="card-body">
+            <h3>Parigi, Francia</h3>
+            <p>Parigi, capitale della Francia, è una metropoli cosmopolita rinomata per la sua storia, arte, moda e gastronomia. Situata sul fiume Senna, la città è un importante centro culturale ed economico, con una ricca offerta di attrazioni, musei, teatri e università. </p>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/reykjavik.jpg" alt="Reykjavik" />
+          </div>
+          <div className="card-body">
+            <h3>Reykjavik, Islanda</h3>
+            <p>Reykjavík è la capitale e la città più grande dell'Islanda, situata nella parte sud-occidentale del paese, sulla costa dell'Oceano Atlantico. È nota per essere la capitale più settentrionale del mondo, ed è un centro politico, culturale ed economico per l'isola. La città, con i suoi circa 140.000 abitanti, offre un mix affascinante di natura, modernità, cultura, storia e paesaggi mozzafiato. </p>
+          </div>
+        </div>
+        <div className="col-3">
+          <div className="card-image">
+            <img src="./img/tokyo.jpg" alt="Tokyo" />
+          </div>
+          <div className="card-body">
+            <h3>Tokyo, Giappone</h3>
+            <p>Tokyo, capitale del Giappone, è una metropoli dinamica e multiforme, rinomata per la sua fusione unica di tradizione e modernità. Offre una vasta gamma di esperienze, dai quartieri iconici come Shinjuku e Shibuya, ai tranquilli giardini imperiali e ai templi antichi. Tokyo è anche un centro globale per la gastronomia, la moda e la tecnologia, con attrazioni per tutti i gusti. </p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
