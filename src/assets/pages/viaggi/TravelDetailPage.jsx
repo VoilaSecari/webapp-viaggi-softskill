50943db4272112c7b8471baf5cfd307101e4f30d
fedebacci <federicobacci1999@gmail.com>
2025-06-24 10:23:11 +0200

    correzione lista partecipanti

--- src/assets/pages/viaggi/TravelDetailPage.jsx
+++ src/assets/pages/viaggi/TravelDetailPage.jsx
@@ -32,90 +32,83 @@ export default function TravelDetailPage() {
         </div>
       </div>
 
-
-
-      <h3>
-        Lista dei contatti (2 versioni: tabella normale o accordion) del viaggio con id: {id}
-      </h3>
-              {
-                clients &&
-                <>
-                  <h2 className="text-center mt-5 mb-1">
-                      Accordion List
-                  </h2>
-                  <div className="accordion" id="accordionList">
-                      {
-                          clients.map((client, index) => {
-                              return (
-                                  <div key={client.id} className="accordion-item">
-                                      <h2 className="accordion-header">
-                                          <button className={`accordion-button ${index === 0 ? "" : " collapsed"}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${client.id}`} aria-expanded={`${index === 0 ? "true" : "false"}`} aria-controls={`collapse${client.id}`}>
-                                              {client.surname} {client.name}
-                                          </button>
-                                      </h2>
-                                      <div id={`collapse${client.id}`} className={`accordion-collapse collapse ${index === 0 ? " show" : ""}`} data-bs-parent="#accordionList">
-                                          <div className="accordion-body">
-                                                <a href={`tel:${client.phone}`} className="btn btn-primary me-1">
-                                                  Chiama subito
-                                              </a>
-                                              <a href="#" className="btn btn-secondary">
-                                                  Visualizza dettagli
-                                              </a>
-                                          </div>
-                                      </div>
-                                  </div>
-                              )
-                          })
-                      }
-                  </div>
-
-                  <h2 className="text-center mt-5 mb-1">
-                      Table list
-                  </h2>
-                  <table className="table table-striped table-bordered table-hover" id="tableList">
-                      <thead>
-                          <tr>
-                              <th scope="col">
-                                  #ID
-                              </th>
-                              {/* <th scope="col w-100"> */}
-                              <th scope="col w-100" style={{width: "100%"}}>
-                                  Name
-                              </th>
-                              <th scope="col">
-                                  Actions
-                              </th>
-                          </tr>
-                      </thead>
-                      <tbody>
-                        {
-                          clients.map((client, index) => {
+            {
+              clients &&
+              <>
+                <h2 className="text-center mt-5 mb-1">
+                    Accordion List
+                </h2>
+                <div className="accordion" id="accordionList">
+                    {
+                        clients.map((client, index) => {
                             return (
-                              <tr key={client.id}>
-                                  <th scope="row">
-                                    {client.id}
-                                  </th>
-                                  <td>
-                                    {client.surname} {client.name}
-                                  </td>
-                                  <td>
-                                      <div className="text-nowrap">
-                                          <a href={`tel:${client.phone}`} className="btn btn-primary me-1">
-                                            Chiama subito
-                                          </a>
-                                          {/* <a href="#" className="btn btn-secondary"> */}
-                                          <a href={`/viaggi/${id}/partecipanti`} className="btn btn-secondary">
-                                            Visualizza dettagli
-                                          </a>
-                                      </div>
-                                  </td>
-                              </tr>
+                                <div key={client.id} className="accordion-item">
+                                    <h2 className="accordion-header">
+                                        <button className={`accordion-button ${index === 0 ? "" : " collapsed"}`} type="button" data-bs-toggle="collapse" data-bs-target={`#collapse${client.id}`} aria-expanded={`${index === 0 ? "true" : "false"}`} aria-controls={`collapse${client.id}`}>
+                                            {client.surname} {client.name}
+                                        </button>
+                                    </h2>
+                                    <div id={`collapse${client.id}`} className={`accordion-collapse collapse ${index === 0 ? " show" : ""}`} data-bs-parent="#accordionList">
+                                        <div className="accordion-body">
+                                              <a href={`tel:${client.phone}`} className="btn btn-primary me-1">
+                                                Chiama subito
+                                            </a>
+                                            <a href={`/viaggi/${id}/partecipanti`} className="btn btn-secondary">
+                                                Visualizza dettagli
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
                             )
-                          })
-                        }
-                      </tbody>
-                  </table>
-                </>
+                        })
+                    }
+                </div>
+
+                <h2 className="text-center mt-5 mb-1">
+                    Table list
+                </h2>
+                <table className="table table-striped table-bordered table-hover" id="tableList">
+                    <thead>
+                        <tr>
+                            <th scope="col">
+                                #ID
+                            </th>
+                            <th scope="col w-100" style={{width: "100%"}}>
+                                Name
+                            </th>
+                            <th scope="col">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        clients.map((client, index) => {
+                          return (
+                            <tr key={client.id}>
+                                <th scope="row">
+                                  {client.id}
+                                </th>
+                                <td>
+                                  {client.surname} {client.name}
+                                </td>
+                                <td>
+                                    <div className="text-nowrap">
+                                        <a href={`tel:${client.phone}`} className="btn btn-primary me-1">
+                                          Chiama subito
+                                        </a>
+                                        <a href={`/viaggi/${id}/partecipanti`} className="btn btn-secondary">
+                                          Visualizza dettagli
+                                        </a>
+                                    </div>
+                                </td>
+                            </tr>
+                          )
+                        })
+                      }
+                    </tbody>
+                </table>
+              </>
             }
     </div>
   );
